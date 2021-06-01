71995065bb63a40cc1a8e1f550a6287fe63989dd
coookiedam <coookiedam@gmail.com>
2021-06-01 22:27:57 +0900

    onoff버튼 수정

---
+++ app/src/main/java/com/example/iotproject/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.iotproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
